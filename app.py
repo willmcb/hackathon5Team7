a262b6e49bd0d6fcee710bfcd71e2e93825c22a2
Cliff Xuan <cliff.xuan@gmail.com>
2016-11-23 14:50:43 +0000

    search by case number

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import requests
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 from flask_restful import Resource, Api, reqparse, abort
+
+import os
+
+PWD = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,7 +19,7 @@ CLAIM_ENDPOINT = '{}/claims'.format(BASE, KEY)
 
 parser = reqparse.RequestParser()
 parser.add_argument(
-    'search', type=str,
+    'caseNumber', type=str,
     help='Rate to charge for this resource')
 
 
@@ -25,11 +29,11 @@ class Adaptor(Resource):
         try:
             args = parser.parse_args()
             data = requests.get(
-                CLAIMS_ENDPOINT + '&search=' + args.search).json()
+                CLAIMS_ENDPOINT + '&search=' + args.caseNumber).json()
             print(data)
             item = data['items'][0]
             case_number = item['case_number']
-            if case_number != args.search:
+            if case_number != args.caseNumber:
                 raise Exception
             uuid = item['uuid']
             url = CLAIM_ENDPOINT + '/' + uuid + '?api_key={}'.format(KEY)
@@ -39,7 +43,28 @@ class Adaptor(Resource):
             return abort(404, message='Not Found')
 
 
-api.add_resource(Adaptor, '/')
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+
+@app.route('/bootstrap-3.3.7/<path:path>')
+def send_bootstrap(path):
+    return send_from_directory('bootstrap-3.3.7', path)
+
+
+@app.route('/')
+def search_html():
+    # ff = os.path.join(PWD, 'searchPage.html')
+    return render_template('searchPage.html')
+
+
+api.add_resource(Adaptor, '/search')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- js/query.js
+++ js/query.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
-  var baseURL = 'https://staging-adp.dsd.io/api/case_workers/claims?api_key=bbef1c5f-0ded-43d2-8d53-5a6358659dac&status=unallocated&scheme=agfs&filter=all&sorting=id&direction=asc&limit=10';
-  $.get(baseURL + 'search=T20150040', function(data) {
-    console.log(data);
-  });
+  function search(caseNumber) {
+    var baseURL = '/search';
+    $.get(baseURL + '?search=' + caseNumber, function(data) {
+      console.log(data);
+    });
+  }
 });
